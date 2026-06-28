d65bf3946e54da682408b7820657305c259d25b1
Umme Hani Shifa <ummehanishifa2227@email.com>
2026-06-28 14:56:28 +0530

    Add SQL queries for DAU and status change analysis

--- answer.sql
+++ answer.sql
@@ -1,13 +1,21 @@
--- TYPE YOUR SQL QUERY BELOW
+-- PART 1: Daily average users before and after the feature change (launched 2018-06-02)
+SELECT
+  DATE(login_timestamp, 'unixepoch') AS day,
+  COUNT(DISTINCT user_id) AS daily_active_users,
+  CASE
+    WHEN DATE(login_timestamp, 'unixepoch') < '2018-06-02'
+    THEN 'before'
+    ELSE 'after'
+  END AS period
+FROM login_history
+GROUP BY day
+ORDER BY day;
 
--- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
-
-
-
-
--- PART 2: Create a SQL query that indicates the number of status changes by card
-
-
-
-
-
+-- PART 2: Number of status changes by card
+SELECT
+  cardID,
+  COUNT(*) AS status_changes
+FROM card_change_history
+WHERE oldStatus != '' AND newStatus != oldStatus
+GROUP BY cardID
+ORDER BY status_changes DESC;
